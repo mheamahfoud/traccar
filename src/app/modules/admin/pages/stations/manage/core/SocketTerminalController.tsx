576caf6c57fc5e4d444b59f738f4a46d71dadecc
mheamahfoud <mohea125.ali@gmail.com>
2023-05-31 16:51:10 +0300

    commit

--- src/app/modules/admin/pages/stations/manage/core/SocketTerminalController.tsx
+++ src/app/modules/admin/pages/stations/manage/core/SocketTerminalController.tsx
@@ -38,10 +38,10 @@ const SocketTerminalController = () => {
   const connectSocket = () => {
     const protocol = window.location.protocol === "https:" ? "wss:" : "ws:";
 
-    const socket = new WebSocket(
-      `${protocol}//${window.location.host}/api/socket`
-    );
-  //const socket = new WebSocket("ws://173.249.51.233:8082/api/socket");
+    // const socket = new WebSocket(
+    //   `${protocol}//${window.location.host}/api/socket`
+    // );
+  const socket = new WebSocket("ws://173.249.51.233:8082/api/socket");
 
     socketRef.current = socket;
 
